8073dfa0f77990e1521cab008045022f2d2a527e
Maurice Ammour <maurice.ammour@informatik.hs-fulda.de>
2020-12-03 18:11:38 +0100

    Updating Spielfeld

--- VierGewinnt/src/Logik/VierGewinnt.java
+++ VierGewinnt/src/Logik/VierGewinnt.java
@@ -1,15 +1,21 @@
 package Logik;
-
+import java.util.Scanner;
 public class VierGewinnt {
 
 	public static String Spielfeld(int feld) {
-		System.out.println("");
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		System.out.println("|__|__|__|__|__|__|__|\n");
+		Scanner s = new Scanner(System.in);
+		String[] eingabe = s.nextString();
+		System.out.println("\n");
+		for(int i = 0; i < 6; i++) {
+			System.out.println("|__|__|__|__|__|__|__|\n");
+		}
+//		System.out.println("");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
+//		System.out.println("|__|__|__|__|__|__|__|\n");
 		return "";
 		// in Felder müssen Plätze von Array initialisert werden damit wir in die Felder schreiben können d.h. brauchen Array mit 0 - 41 Stellen
 	}
