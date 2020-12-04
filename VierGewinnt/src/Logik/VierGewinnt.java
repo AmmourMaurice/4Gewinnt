2e5b6a041dc7e80127e494128398b215d03bf7cd
Janik Klaus Wagner <janik-klaus.wagner@informatik.hs-fulda.de>
2020-12-04 15:54:34 +0100

    edit Spielfeld

--- VierGewinnt/src/Logik/VierGewinnt.java
+++ VierGewinnt/src/Logik/VierGewinnt.java
@@ -1,33 +1,33 @@
 package Logik;
 import java.util.Scanner;
-public class VierGewinnt {
+public class VierGewinnt{
 
-	public static String Spielfeld(int feld) {
-		Scanner s = new Scanner(System.in);
-		String[] eingabe = s.nextString();
-		System.out.println("\n");
-		for(int i = 0; i < 6; i++) {
-			System.out.println("|__|__|__|__|__|__|__|\n");
-		}
+    public static void gamefield(char[][] spielfeld){
+        System.out.println("");
+        System.out.println(" 1 2 3 4 5 6 7");
+        System.out.println("_______________");
+        for (int reihe = 0; reihe < spielfeld.length; reihe++){
+            System.out.print("|");
+            for (int strich = 0; strich < spielfeld[0].length; strich++){
+                System.out.print(spielfeld[reihe][strich]);
+                System.out.print("|");
+            }
+            System.out.println();
+            System.out.println("_______________");
+        }
+        System.out.println(" 1 2 3 4 5 6 7");
+        System.out.println();
 
-		String[42] eingabe = s.nextString();
-		System.out.println("|__|__|__|__|__|__|__|\n");
-		
-		
+    }
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        char[][] spielfeld = new char[6][7];
 
-//		System.out.println("");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-//		System.out.println("|__|__|__|__|__|__|__|\n");
-		return "";
-		// in Felder müssen Plätze von Array initialisert werden damit wir in die Felder schreiben können d.h. brauchen Array mit 0 - 41 Stellen
-	}
-	public static void main(String[] args) {
-		Spielfeld(6);
-
-	}
-
+        for (int reihe = 0; reihe < spielfeld.length; reihe++){
+            for (int strich = 0; strich < spielfeld[0].length; strich++){
+                spielfeld[reihe][strich] = ' ';
+            }
+        }
+        gamefield(spielfeld);
+    }
 }
