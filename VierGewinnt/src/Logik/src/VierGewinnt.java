9a4a4d981744b344b5c03275e1b8897c7f7d24a3
Tobias Roland Ritter <tobias-roland.ritter@informatik.hs-fulda.de>
2020-12-27 11:40:27 +0100

    gameloop und spielerwechsel

--- VierGewinnt/src/Logik/src/VierGewinnt.java
+++ VierGewinnt/src/Logik/src/VierGewinnt.java
@@ -1,15 +1,15 @@
-iimport java.util.Scanner;
+import java.util.Scanner;
 public class VierGewinnt{
 
-    
-    public static void gamefield(char[][] spielfeld){
+
+    public static void erzeugeSpielfeld(char[][] spielfeld){
         System.out.println("");
         System.out.println(" 0 1 2 3 4 5 6");
         System.out.println("_______________");
-        for (int reihe = 0; reihe < spielfeld.length; reihe++){
+        for (int zeile = 0; zeile < spielfeld.length; zeile++){
             System.out.print("|");
-            for (int strich = 0; strich < spielfeld[0].length; strich++){
-                System.out.print(spielfeld[reihe][strich]);
+            for (int spalte = 0; spalte < spielfeld[0].length; spalte++){
+                System.out.print(spielfeld[zeile][spalte]);
                 System.out.print("|");
             }
             System.out.println();
@@ -17,48 +17,112 @@ public class VierGewinnt{
         }
         System.out.println(" 0 1 2 3 4 5 6");
         System.out.println();
-
     }
-    
-    public static int eingabe(int player) { 
-        System.out.println("Geben Sie die FeldNr ein um ihren Stein zu platzieren:");
-        Scanner s = new Scanner(System.in);
-        int feld = s.nextInt();
-        return feld;
-    }
-    
-    public static char[][] steinPlatzieren(char[][] spielfeld, int spalte, int player){
-        char stein;
-        if(player == 1) {
-            stein = 'O';
-        }else {
-            stein = 'X';
-        }
-        spielfeld[5][spalte] = stein;
-        if(spielfeld[5][5] == 'O' || spielfeld[5][5] == 'X') {
-            spielfeld[5][stein] = spielfeld[4][stein]; //stein muss getauscht werden warscheinlich für platz der zeile in spalte 4
-        }
-            
+    public static void spielzug(char[][] spielfeld,int player, int runde) {
+        Scanner eingabe = new Scanner(System.in);
+        erzeugeSpielfeld(spielfeld);
+        System.out.println("Spieler " + player + " wähle eine Spalte aus");
+        int play = eingabe.nextInt();
         
-        return spielfeld;
+        if (spielzugBestaetiegen(spielfeld, play) == true) {
+        if (player ==1) {
+        	
+        	spielfeld[5][play] = 'X';
+        }else { spielfeld[5][play] = 'O';
+        }
+        erzeugeSpielfeld(spielfeld);
+        
+        	}
+        
+        else {
+        		if (fallUnterscheidungImSpielzug(spielfeld, play)==false ) {
+        			//while ((spielzugBestaetiegen(spielfeld, play)) == false){
+        			System.out.println("Bitte geben sie eine gültige Zahl ein.");
+        			play = eingabe.nextInt();
+        			 if (spielzugBestaetiegen(spielfeld, play) == true) {
+        				 if (player ==1) {
+        			        	
+        			        	spielfeld[5][play] = 'X';
+        			        }else { spielfeld[5][play] = 'O';
+        			       
+        			        erzeugeSpielfeld(spielfeld);
+        			        
+        			        }
+        			 	}
+        		//	}	
+        			
+        		}else 
+        	{
+        			while ((spielzugBestaetiegen(spielfeld, play)) == true){
+        					int i =1;
+        					if (player ==1) {
+        			        	
+        			        	spielfeld[5-i][play] = 'X';
+        			        }else { spielfeld[5-i][play] = 'O';
+        			       
+        			        erzeugeSpielfeld(spielfeld);
+        			        i++;
+        				}
+        			}
+        		}
+        	}
+        }
+        
+        		 		
+    		
+        	
+        	
+        	 //Abfrage welche der beiden Bedingungen falsch ist und dann entweder Wiederholung oder Korrektur nach oben
+        	
+   
+        
+    
+    
+    public static boolean spielzugBestaetiegen(char[][] spielfeld, int play ) {
+    	if(play < 0 || play > 6 ) {	
+    		return false;
+        }
+        else {
+            if(spielfeld[0][play] == ' ') {	
+            	return false;
+            }
+            return true;
+        }
     }
     
+    
+    public static boolean fallUnterscheidungImSpielzug (char[][] spielfeld, int play) {
+    	if(play < 0 || play > 6 ) {	
+    		return false;
+        }
+        else {
+            	return true;
+    	
+    }
+  }
     public static void main (String[] args) {
         char[][] spielfeld = new char[6][7];
         int player = 1;
-        int spielzug = 1;
+        int runde = 1;
         boolean gewinner = false;
+        
+        for (int zeile = 0; zeile < spielfeld.length; zeile++){
+			for (int spalte = 0; spalte < spielfeld[0].length; spalte++){
+				spielfeld[zeile][spalte] = ' ';
+			}
+		}
+        while (gewinner == false || runde>42) {       	        
+        spielzug(spielfeld,player, runde);
        
-        gamefield(spielfeld);
-        int spalte = eingabe(player);
-        spielfeld = steinPlatzieren(spielfeld, spalte, player);
-        gamefield(spielfeld);
         
-        while (gewinner == false || spielzug <=42)
-        {
-
+        if (player == 1){
+			player = 2;
+		}else{
+			player = 1;
+		}
+		
+		runde++;
         }
-        
-        
+
     }
-}
+} 
